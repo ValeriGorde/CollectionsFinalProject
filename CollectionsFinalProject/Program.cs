306a37b3b3c1b824e45537f83062d0006b8725a4
Лера Гордеева <valeri.gorde@mail.ru>
2022-09-21 21:25:54 +0300

    Добавьте файлы проекта.

---
+++ CollectionsFinalProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Diagnostics;
+
+namespace Task1
+{
+    internal class Program
+    {
+        public const string filePath = "Text1.txt";
+        static void Main(string[] args)
+        {
+            string allText = File.ReadAllText(filePath);
+            
+
+            List<string> text = new List<string>();
+            LinkedList<string> linkedText = new LinkedList<string>();
+
+            //Вставка текста в List<string>
+            var watchTwo = Stopwatch.StartNew();
+            text.Add(allText);
+
+
+            //Вставка текста в LinkedList<string>
+            //var watchTwo = Stopwatch.StartNew();
+            //linkedText.AddLast(allText);
+
+            watchTwo.Stop();
+            Console.WriteLine($"Вставка текста в список: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+
+            //0,0248мс в ListLinked<string>
+            //0,0066мс в  List<string>
+            //Следовательно, вставка текста в List<>, занимает примерно в 3 раза меньше времени, чем в ListLinked<>.
+
+        }
+
+
+    }
+}
---
+++ Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 0;
+            string filePath = @"D:\Загрузки\Text1.txt";
+            string allText = File.ReadAllText(filePath);
+            var noPunctuationText = new string(allText.Where(c => !char.IsPunctuation(c)).ToArray());
+            
+            char[] delimiters = new char[] { ' ', '\r', '\n' };
+            var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+            Dictionary<string, int> text = new Dictionary<string, int>();
+
+            
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (text.ContainsKey(words[i]))
+                {
+                    text[words[i]] += 1;
+                }
+                else
+                    text[words[i]] = 1;
+            }
+
+            text = text.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+            size = text.Count;
+
+            Console.WriteLine("Десять слов, которые чаще всего встречаются в тексте: ");
+            foreach (var word in text)
+            {
+                if(size < 10) 
+                {
+                    Console.WriteLine(word.Key + " - " + word.Value + " раз");
+                }
+                size--;
+                
+            }
+        }
+    }
+}
